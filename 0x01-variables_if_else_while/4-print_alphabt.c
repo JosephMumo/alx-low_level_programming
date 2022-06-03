b3df208c9357de5e9fa059abe6af42ad4d9c21b9
JosephMumo <josephmumo25@gmail.com>
2022-06-03 06:09:09 -0700

    fourth commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all alphabets except e and q
+ * Return: 0 if executed successfull
+ */
+int main(void)
+{
+	char xy = 'a';
+
+	while (xy <= 'z')
+	{
+		if (xy != 'e' && xy != 'z')
+		{
+			putchar(xy);
+		}
+		xy++;
+	}
+	putchar('\n');
+	return (0);
+}
