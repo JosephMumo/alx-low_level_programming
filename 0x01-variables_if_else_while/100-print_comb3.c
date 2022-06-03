a57599e1232f8657ffe360f19a0aeeacc7fbb742
JosephMumo <josephmumo25@gmail.com>
2022-06-03 07:56:26 -0700

    100 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 48; x <= 57; x++)
+	for (x = 48; x <= 56; x++)
 	{
-		for (y = 48; y <= 57; y++)
+		for (y = 49; y <= 57; y++)
 		{
 			putchar(x);
 			putchar(y);
-			if (x != 57 || y != 57)
+			if (x != 56 || y != 57)
 			{
 				putchar(',');
 				putchar(' ');
