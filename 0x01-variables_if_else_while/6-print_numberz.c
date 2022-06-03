1fdfdbc78674642f85e86a6cf10669770735e38a
JosephMumo <josephmumo25@gmail.com>
2022-06-03 06:50:19 -0700

    6th commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1 to 10
+ * Return: 0 if successfully executed
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
