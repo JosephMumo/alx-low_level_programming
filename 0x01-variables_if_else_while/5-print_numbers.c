44a1ff682d0ac1b0ee0fea0a94baade460594814
JosephMumo <josephmumo25@gmail.com>
2022-06-03 06:27:32 -0700

    fifth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 numbers,
+ * Return: 0 if executed correctly
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
