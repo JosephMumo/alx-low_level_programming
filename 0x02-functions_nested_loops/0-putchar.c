a54219f925e92b78ef62de5f4612dcf2b56f518f
JosephMumo <josephmumo25@gmail.com>
2022-06-07 04:39:23 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints characters c to stdout
+ * @: The characters to be printed
+ *
+ * Return: 1 if executed successfully
+ * On error, -1 if error is present
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory containds c programming exercises to practice: Nested loops, Functions, function prototypes and header files
