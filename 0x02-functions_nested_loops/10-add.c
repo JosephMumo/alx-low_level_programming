6878515cc4f60ff65a16c0c8018ae3ad19276a75
JosephMumo <josephmumo25@gmail.com>
2022-06-07 09:38:01 -0700

    11th commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two ints
+ * @a: first computation int
+ * @b: second computation int
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
