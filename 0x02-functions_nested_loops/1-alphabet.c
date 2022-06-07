b7ef43626e6ddddb31ec1a2699b051d9c4821441
JosephMumo <josephmumo25@gmail.com>
2022-06-07 06:51:15 -0700

    second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * Return: nothing if executed successfully
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
