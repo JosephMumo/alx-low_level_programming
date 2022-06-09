8e38b1fd56b41576ba4adb3062cf360b08ca6904
JosephMumo <josephmumo25@gmail.com>
2022-06-09 04:22:11 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 except 2&4
+ * Return: results
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
