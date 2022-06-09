7afe7098b30d8ce6f67e707116b50d665d8878cb
JosephMumo <josephmumo25@gmail.com>
2022-06-09 04:05:03 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Return: nothing to return here
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c < '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
