08780fae2875e7cb1a3daa34e4d1351fe2aa6c2f
JosephMumo <josephmumo25@gmail.com>
2022-06-20 05:35:44 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches the string for any number of bytes
+ * @s: pointer to the param
+ * @accept: pointer to char
+ * Return: NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++;)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
