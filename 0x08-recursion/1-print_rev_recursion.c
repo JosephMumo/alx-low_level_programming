1337e95918a30ed28656e168472f603ab2dd0544
JosephMumo <josephmumo25@gmail.com>
2022-06-22 05:39:25 -0700

    recursions

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to the string
+ * Return: nothing to return here
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(--*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
