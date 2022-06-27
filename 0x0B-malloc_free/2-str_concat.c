e4680ac3021dd2b9162b036ca5d2b2f8af851be5
JosephMumo <josephmumo25@gmail.com>
2022-06-27 08:08:46 -0700

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: char param
+ * Return: str length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+
+/**
+ * str_concat - concat two str
+ * @s1: str1
+ * @s2: str2
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int l1, l2;
+	char *conc, *tmp;
+
+	if (!s1)
+		s1 = "";
+	else
+		l1 = _strlen(s1);
+
+	if (!s2)
+		s2 = "";
+	else
+		l2 = _strlen(s2);
+
+	conc = malloc(l1 + l2 + 1);
+	if (!conc)
+		return (0);
+
+	tmp = conc;
+	while (*s1)
+		*tmp++ = *s1++;
+
+	while ((*tmp++ = *s2++))
+		;
+	return (conc);
+}
