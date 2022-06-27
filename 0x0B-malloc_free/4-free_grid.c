0b68eabbaa29c6e856aa054a1688f58bc649d744
JosephMumo <josephmumo25@gmail.com>
2022-06-27 09:38:55 -0700

    malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - alloc_grid
+ * @grid: pointer
+ * @height: array
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	height--;
+
+	while (height >= 0)
+	{
+		free(*(grid + height));
+		height--;
+	}
+	free(grid);
+}
