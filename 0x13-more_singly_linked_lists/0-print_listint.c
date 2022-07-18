6d39572c9b3af179c236986f425cb4af323bcdb8
JosephMumo <josephmumo25@gmail.com>
2022-07-18 03:57:49 -0700

    more lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a linked list
+ * @h: linked list to print
+ * Return: nodes number
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
